c9c6e897422269a9dfe34e1a39f159617aacacb0
Paul Saikko <paul.saikko@gmail.com>
2020-02-09 11:22:49 +0200

    Add equality comparison stubs, make constructor explicit

--- polynomial.h
+++ polynomial.h
@@ -12,7 +12,7 @@ class Polynomial {
  public:
     Polynomial() = default;
 
-    Polynomial(std::map<unsigned, T> terms) {
+    explicit Polynomial(std::map<unsigned, T> terms) {
         for (auto &p : terms) {
             if (p.second != (T)0) {
                 this->terms[p.first] = p.second;
@@ -42,6 +42,14 @@ class Polynomial {
 
     Polynomial<T> operator* (Polynomial<T> const &other) {
         return other;
+    }
+
+    bool operator== (Polynomial<T> const &other) const {
+        return false;
+    }
+
+    bool operator!= (Polynomial<T> const &other) const {
+        return !(*this == other);
     }
 
     void print(std::ostream& os, std::string variable="x") {
